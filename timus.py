5655250e965928a8720ce1f6b6192d30dd8afec0
Tomasz <tomek@komp.lan>
2015-07-12 19:33:27 +0100

    Fixed xdg-open

--- timus.py
+++ timus.py
@@ -7,14 +7,14 @@ from lxml.cssselect import CSSSelector
 def main():
 	problem_id = sys.argv[1]
 	sel = CSSSelector('.sample')
-	os.system("xdg-open http://acm.timus.ru/problem.aspx?space=1&num="+problem_id)
+	os.system("xdg-open http://acm.timus.ru/problem.aspx?space=1\&num="+problem_id)
 	if not os.path.exists(problem_id):
 		os.makedirs(problem_id)
 	res = requests.get("http://acm.timus.ru/problem.aspx?space=1&num="+problem_id)
 	root = etree.fromstring(res.content, etree.HTMLParser())
 	cnt = 0
 	# for i in .getchildren()[1:]:
-	
+
 	for i in root.xpath('//table[last()]/tr[position()>1]/td/pre/text()'):
 		if cnt%2==0:
 			f = open(problem_id+"/in"+str(cnt/2),'w+')
@@ -27,11 +27,11 @@ def main():
 		cnt +=1
 	f = open(problem_id+"/test.sh", "w+")
 	f.write("#!/usr/bin/env bash \n")
-	
+
 	for i in range(cnt/2):
 		f.write("./src < in"+str(i)+" > tempout\n")
 		f.write("diff --strip-trailing-cr tempout out"+str(i)+"\n")
 	f.close()
-	
+
 if __name__ == '__main__':
 	main()
